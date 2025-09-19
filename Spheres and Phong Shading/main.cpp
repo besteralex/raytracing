bd755b808817495ea08033db063c3635e9a4e1cd
Alexander Schramm <aschramm@student.ethz.ch>
2025-09-19 16:12:09 +0200

    Add ray-sphere intersections

--- Spheres and Phong Shading/main.cpp
+++ Spheres and Phong Shading/main.cpp
@@ -66,3 +66,82 @@ public:
 		this->material = material;
 	}
 };
+
+/**
+ Implementation of the class Object for sphere shape.
+ */
+class Sphere : public Object{
+private:
+    float radius; ///< Radius of the sphere
+    glm::vec3 center; ///< Center of the sphere
+
+public:
+	/**
+	 The constructor of the sphere
+	 @param radius Radius of the sphere
+	 @param center Center of the sphere
+	 @param color Color of the sphere
+	 */
+    Sphere(float radius, glm::vec3 center, glm::vec3 color) : radius(radius), center(center){
+		this->color = color;
+    }
+	Sphere(float radius, glm::vec3 center, Material material) : radius(radius), center(center){
+		this->material = material;
+	}
+	/** Implementation of the intersection function*/
+	Hit intersect(Ray ray){
+		
+		Hit hit;
+		hit.hit = false;
+
+		// Project the sphere center onto the ray.
+
+		glm::vec3 to_center = this->center - ray.origin;
+		glm::vec3 sphere_center = this->center;
+		glm::vec3 ray_direction = ray.direction;
+		double center_projection = glm::dot(to_center, ray_direction);
+		double center_distance = glm::length(to_center);
+		double distance_to_ray = glm::sqrt(glm::pow(center_distance, 2) - glm::pow(center_projection, 2));
+		double sphere_radius = (double) this->radius;
+		
+		if(distance_to_ray < sphere_radius) {
+			double half_chord_length = glm::sqrt(glm::pow(sphere_radius, 2) - glm::pow(distance_to_ray, 2));
+
+			// The two distances describe the far and near sphere intersections.
+			double far_distance = center_projection + half_chord_length;
+			double near_distance = center_projection - half_chord_length;
+
+			// Ignore intersections behind the ray origin.
+			if(near_distance < 0 && far_distance < 0) {
+				hit.hit = false;
+				return hit;
+			} 	else if(near_distance > 0) {
+				
+				hit.distance = near_distance;
+				hit.intersection = ray.origin + ray_direction * (float)near_distance;
+				hit.hit = true;
+				hit.normal = (hit.intersection - sphere_center);
+				hit.object = this;
+			} else if (far_distance > 0) {
+				hit.distance = far_distance;
+				hit.intersection = ray.origin + ray_direction * (float)far_distance;
+				hit.hit = true;
+				hit.normal = (hit.intersection - sphere_center);
+				hit.object = this;
+			}
+			
+		} else if (distance_to_ray == sphere_radius) {
+			if(center_projection < 0) {
+				return hit;
+			}
+
+			hit.distance = center_projection;
+			hit.intersection = ray.origin + ray_direction * (float)center_projection;
+			hit.hit = true;
+			
+			hit.normal = glm::normalize(hit.intersection - sphere_center);
+			hit.object = this;
+	}	
+		return hit;
+	}
+};
