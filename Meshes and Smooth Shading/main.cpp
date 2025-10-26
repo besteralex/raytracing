4e2b85e20071b6506545ed0b84c96fc7c60e2ead
Alexander Schramm <aschramm@student.ethz.ch>
2025-10-26 14:18:47 +0100

    Render the mesh scene with tone mapping

--- Meshes and Smooth Shading/main.cpp
+++ Meshes and Smooth Shading/main.cpp
@@ -529,3 +529,63 @@ void sceneDefinition (){
 	}
 	
 }
+
+glm::vec3 toneMapping(glm::vec3 intensity){
+	float gamma = 0.5f;
+	float alpha = 12.0f;
+	return glm::clamp(alpha * glm::pow(intensity, glm::vec3(gamma)), glm::vec3(0.0), glm::vec3(1.0));
+}
+
+
+
+int main(int argc, char * argv[]) {
+
+    clock_t render_ticks = clock();
+
+    int width = 1024;
+    int height = 768;
+    float field_of_view = 90;
+
+	sceneDefinition();
+
+	Image image(width,height);
+	vector<glm::vec3> image_values(width*height);
+
+    float pixel_size = 2*tan(0.5*field_of_view/180*M_PI)/width;
+    float image_left = -pixel_size * width / 2;
+    float image_top = pixel_size * height / 2;
+
+    for(int pixel_x = 0; pixel_x < width ; pixel_x++) {
+        for(int pixel_y = 0; pixel_y < height ; pixel_y++){
+
+			// Aim each ray through the center of its pixel.
+			float ray_x = image_left + pixel_x*pixel_size + pixel_size/2;
+            float ray_y = image_top - pixel_y*pixel_size - pixel_size/2;
+            float ray_z = 1;
+
+			glm::vec3 camera_origin(0, 0, 0);
+            glm::vec3 direction(ray_x, ray_y, ray_z);
+            direction = glm::normalize(direction);
+
+            Ray ray(camera_origin, direction);
+
+            image.setPixel(pixel_x, pixel_y, toneMapping(trace_ray(ray)));
+        }
+	}
+	
+    render_ticks = clock() - render_ticks;
+	cout<<"It took " << ((float)render_ticks)/CLOCKS_PER_SEC<< " seconds to render the image."<< endl;
+    cout<<"I could render at "<< (float)CLOCKS_PER_SEC/((float)render_ticks) << " frames per second."<<endl;
+
+	// Save the image to the requested path, or use result.ppm.
+	if (argc == 2){
+		image.writeImage(argv[1]);
+		std::cout << "Image written to " << argv[1] << std::endl;
+	}else{
+		image.writeImage("./result.ppm");
+		std::cout << "Image written to ./result.ppm" << std::endl;
+	}
+	
+    return 0;
+}
+
