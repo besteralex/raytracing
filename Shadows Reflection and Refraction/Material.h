62a706cd6fca1e40d86b63634c3381d41e8310e5
Alexander Schramm <aschramm@student.ethz.ch>
2025-10-29 18:52:13 +0100

    Add image output and material settings

---
+++ Shadows Reflection and Refraction/Material.h
@@ -0,0 +1,26 @@
+//
+//  Material.h
+//  Raytracer
+//
+//  Created by Piotr Didyk on 14.07.21.
+//
+
+#ifndef Material_h
+#define Material_h
+
+#include "glm/glm.hpp"
+
+/**
+ Structure describing a material of an object
+ */
+struct Material{
+    glm::vec3 ambient = glm::vec3(0.0);
+    glm::vec3 diffuse = glm::vec3(1.0);
+    glm::vec3 specular = glm::vec3(0.0);
+    float shininess = 0.0;
+    bool does_reflect = false;
+    bool does_refract = false;
+    float refractive_index = 2.0;
+};
+
+#endif /* Material_h */
